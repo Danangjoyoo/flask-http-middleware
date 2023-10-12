5997cd159d35e5484ac83336a6461f03e09812d4
Agus Danangjoyo <68761014+Danangjoyoo@users.noreply.github.com>
2023-10-13 00:13:27 +0700

    build: 0.4.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from pathlib import Path
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
-VERSION = "0.3.1"
+VERSION = "0.4.0"
 DESCRIPTION = "A module to create middleware with direct access to `request` and `response`"
 
 # Setting up
